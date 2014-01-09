c81b9cf04b4a482b1a85b9307e2f0b469df6470e
wangyi <tririverwangyi@gmail.com>
2014-01-09 13:31:43 +0000

    Bumped year into 2014

--- MathGR.m
+++ MathGR.m
@@ -1,4 +1,4 @@
 (* Mathematica package *)
 BeginPackage["MathGR`MathGR`", {"MathGR`tensor`", "MathGR`decomp`", "MathGR`gr`", "MathGR`ibp`", "MathGR`typeset`"}]
-Print["MathGR, by Yi Wang (2013), https://github.com/tririver/MathGR. \nBugs can be reported to https://github.com/tririver/MathGR/issues\nLoaded components tensor, decomp, gr, ibp, typeset."]
+Print["MathGR, by Yi Wang (2013, 2014), https://github.com/tririver/MathGR. \nBugs can be reported to https://github.com/tririver/MathGR/issues\nLoaded components tensor, decomp, gr, ibp, typeset."]
 EndPackage[]
